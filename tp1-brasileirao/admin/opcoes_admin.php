7f155a94d339480c70bb8651d598a571f943052f
isacsandim@gmail.com <isacsandim@gmail.com@39385b1a-61bc-3cca-02f7-3c442251d249>
2011-10-02 20:48:19 +0000

    adicionando imagens times

--- tp1-brasileirao/admin/opcoes_admin.php
+++ tp1-brasileirao/admin/opcoes_admin.php
@@ -3,6 +3,10 @@
 <p align="center"><b>Menu</b></p>
 
 <ul>
-	<li><a href="?acao=incluir">Incluir</a></li>
+	<li>
+            <ul>
+                <li><a href="?acao=incluir">Incluir</a></li>
+            </ul>
+        </li>
 	<li><a href="?acao=listar">Listar</a></li>
 </ul>
