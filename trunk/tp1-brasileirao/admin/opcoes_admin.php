d74b9cecb2a83ee603d92899f76932783e6faf04
isacsandim@gmail.com <isacsandim@gmail.com@39385b1a-61bc-3cca-02f7-3c442251d249>
2011-10-07 11:58:29 +0000

    novas versoes de arquivos administrativos

--- trunk/tp1-brasileirao/admin/opcoes_admin.php
+++ trunk/tp1-brasileirao/admin/opcoes_admin.php
@@ -2,19 +2,19 @@
 <p align="center"><b>Menu</b></p>
 
 <ul>
-   <li><a href="?acao=incluir">Incluir Time</a></li>
+   <li><a href="?acao=incluir_time">Incluir Time</a></li>
    <li><a href="?acao=listar_times">Listar Times</a></li>
-   <li><a href="?acao=incluir">Incluir Jogador</a></li>
+   <li><a href="?acao=incluir_jogador">Incluir Jogador</a></li>
    <li><a href="?acao=listar_jogadores">Listar Jogadores</a></li>
-   <li><a href="?acao=incluir">Incluir Árbitro</a></li>
-   <li><a href="?acao=listar">Listar Árbitros</a></li>
-   <li><a href="?acao=incluir">Incluir Técnico</a></li>
-   <li><a href="?acao=listar">Listar Técnicos</a></li>
-   <li><a href="?acao=incluir">Incluir Partida</a></li>
-   <li><a href="?acao=listar">Listar Partidas</a></li>
-   <li><a href="?acao=incluir">Incluir Estádio</a></li>
-   <li><a href="?acao=listar">Listar Estádios</a></li>
-   <li><a href="?acao=incluir">Incluir Times</a></li>
-   <li><a href="?acao=listar">Listar Times</a></li>            
+   <li><a href="?acao=incluir_arbitro">Incluir Árbitro</a></li>
+   <li><a href="?acao=listar_arbitros">Listar Árbitros</a></li>
+   <li><a href="?acao=incluir_tecnico">Incluir Técnico</a></li>
+   <li><a href="?acao=listar_tecnicos">Listar Técnicos</a></li>
+   <li><a href="?acao=incluir_partida">Incluir Partida</a></li>
+   <li><a href="?acao=listar_partidas">Listar Partidas</a></li>
+   <li><a href="?acao=incluir_estadio">Incluir Estádio</a></li>
+   <li><a href="?acao=listarestadios">Listar Estádios</a></li>
+   <li><a href="?acao=incluir_time">Incluir Times</a></li>
+   <li><a href="?acao=listar_time">Listar Times</a></li>            
    <li><a href="?acao=logout">Logout</a></li>
 </ul>
