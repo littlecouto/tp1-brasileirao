ab2a2f31a626afa8f9cad97513772208485518ef
isacsandim@gmail.com <isacsandim@gmail.com@39385b1a-61bc-3cca-02f7-3c442251d249>
2011-10-08 00:51:24 +0000

    alteracao estrutura

--- tp1-brasileirao/admin/alterar_time.php
+++ tp1-brasileirao/admin/alterar_time.php
@@ -1,26 +1,44 @@
-<? include "verifica.php"; ?>
 <?
-$sql = "SELECT id,nome FROM tecnico ORDER BY nome";
-$rs = mysql_query($sql);
-$table = array();
-$i = 0;
-while ($row = mysql_fetch_array($rs)) {
-    $table[$i++] = $row;
+include "verifica.php";
+
+$id = $_GET["id"];
+$sql = "SELECT * FROM time ORDER BY nome where id='$id'";
+$qry = mysql_query($sql) or die(mysql_error());
+
+if (mysql_num_rows($qry) > 0) {
+    $id = $R->id;
+    $nome = $R->nome;
+    $brasao = $R->brasao;
+    $estado = $R->estado;
+    $gols_pro = $R->gols_pro;
+    $gols_contra = $R->gols_contra;
+    $total_faltas = $R->total_faltas;
+    $estadio_id = $R->estadio_id;
+
+    $sql = "SELECT id,nome FROM tecnico ORDER BY nome";
+    $rs = mysql_query($sql) or die(mysql_error());
+    $table = array();
+    $i = 0;
+    while ($row = mysql_fetch_array($rs)) {
+        $table[$i++] = $row;
+    }
+} else {
+    echo "Tabela sem registros.";
+    die();
 }
 ?>
-<form name="form_incluir" id="form_incluir" method="POST" action="?acao=executa_incluir_time">
+<form name="form_alterar" id="form_alterar" method="POST" action="?acao=executa_alterar_time">
     <table>
         <tr>   
-
             <td>
                 Nome:
             </td>
             <td>
-                <input type="text" name="nome" id="nome" size="50" maxlength="50">
+                <input type="text" name="nome" id="nome" size="50" maxlength="50" value="<?= $nome; ?>">
             </td>
         <tr>
             <td>
-                Visitante:
+                Estado:
             </td>
             <td>
                 <select name="estado">  
@@ -59,24 +77,29 @@ while ($row = mysql_fetch_array($rs)) {
                 Tecnico:
             </td> 
             <td>
-                <select name="tecnico">
+                <select name="Tecnico">
                     <?
                     $i = 0;
                     while ($i < count($table)) {
-                        echo "<option value=\"" . utf8_encode($table[$i]['id']) . "\">" . utf8_encode($table[$i]['nome']) . "</option>\n  ";
+                        if ($i == $tecnico_id) {
+                            echo "<ption value='" . utf8_encode($table[$i]['id']) . "'selected='selected'>" . utf8_encode($table[$i]['nome']) . "</option>\n  ";
+                        } else {
+                            echo "<option value=\"" . utf8_encode($table[$i]['id']) . "\">" . utf8_encode($table[$i]['nome']) . "</option>\n  ";
+                        }
                         $i++;
                     }
                     ?>
                 </select>
             </td>
         </tr>
-            <td></td>
-            <td>
-                <input type="submit" name="Submit" value="Enviar">
-            </td>
+        <td></td>
+        <td>
+            <input type="submit" name="Submit" value="Enviar">
+        </td>
     </table> 
-    <input type="hidden" name="gols_pro" id="gols_pro" value="0">
-    <input type="hidden" name="gols_contra" id="gols_contra" value="0">
-    <input type="hidden" name="total_faltas" id="total_faltas" value="0">
+    <input type="hidden" name="gols_pro" id="gols_pro" value="<?= $gols_pro; ?>">
+    <input type="hidden" name="gols_contra" id="gols_contra" value="<?= $gols_contra; ?>">
+    <input type="hidden" name="total_faltas" id="total_faltas" value="<?= $total_faltas; ?>">
     <input type="hidden" name="brasao" id="brasao" value="sem_banner.jpg" >
 </form>
+
