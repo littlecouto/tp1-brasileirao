7b051751d66963c7d15a1ef37f39141a123b1053
isacsandim@gmail.com <isacsandim@gmail.com@39385b1a-61bc-3cca-02f7-3c442251d249>
2011-10-08 16:21:07 +0000

    adicionando tabela campeonato brasileiro

--- trunk/tp1-brasileirao/admin/exibe.php
+++ trunk/tp1-brasileirao/admin/exibe.php
@@ -34,6 +34,40 @@
              */
 
         </script>
+            <script LANGUAGE="JavaScript">
+    totals =0;
+    function adiciona(){
+    totals++
+        tbl = document.getElementById("tabela")
+ 
+        var novaLinha = tbl.insertRow(-1);
+        var novaCelula;
+ 
+        if(totals%2==0) cl = "#F5E9EC";
+        else cl = "#FBF6F7";
+ 
+        novaCelula = novaLinha.insertCell(0);
+ 
+        novaCelula.style.backgroundColor = cl
+ 
+        novaCelula.innerHTML = "<input type='checkbox' name='chkt' />";
+ 
+        novaCelula = novaLinha.insertCell(1);
+        novaCelula.align = "left";
+        novaCelula.style.backgroundColor = cl;
+        novaCelula.innerHTML = "&nbsp; Linha"+totals+"";
+ 
+        novaCelula = novaLinha.insertCell(2);
+        novaCelula.align = "left";
+        novaCelula.style.backgroundColor = cl;
+        novaCelula.innerHTML = "&nbsp;ops3";
+ 
+        novaCelula = novaLinha.insertCell(3);
+        novaCelula.align = "left";
+        novaCelula.style.backgroundColor =cl;
+        novaCelula.innerHTML = "&nbsp;ops4";
+    }
+    </script>
     </head>
 
     <body>
