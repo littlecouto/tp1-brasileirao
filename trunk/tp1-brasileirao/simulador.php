8af4f0d5a7d3a02e3eaf8d5337c2728a64428624
isacsandim@gmail.com <isacsandim@gmail.com@39385b1a-61bc-3cca-02f7-3c442251d249>
2011-10-09 21:27:44 +0000

    simulador fuck yeah

--- trunk/tp1-brasileirao/simulador.php
+++ trunk/tp1-brasileirao/simulador.php
@@ -38,29 +38,36 @@ $sql = "CREATE TABLE  `tp1_bd`.`time_tmp` (
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8 select * from time";
 $qry = mysql_query($sql) or die(mysql_error());
 
-$sql = "SELECT * FROM partida ORDER BY data";
+$sql = "SELECT 
+        partida.id,
+        rodada,
+        mandante_gols,
+        visitante_gols,
+        concat(time1.nome,'-',time1.estado) as mandante_nome,
+        time1.sigla as mandante_sigla,
+        concat(time2.nome,'-',time2.estado) as visitante_nome,
+        time2.sigla as visitante_sigla,
+        time1.brasao as mandante_brasao,
+        time2.brasao as visitante_brasao,
+        DATE_FORMAT(data,'%d/%m/%Y %H:%i:%s')as data_formatada,
+        IF(data < CURDATE(),'passada','') as passou 
+        FROM partida inner join time as time1 inner join time as time2 where time1.id=partida.mandante_id and time2.id=partida.visitante_id
+        ORDER by data";
 $rs = mysql_query($sql) or die(mysql_error());
 $table = array();
-$i = 0;
 while ($row = mysql_fetch_array($rs)) {
-    $table[$i++] = $row;
-}
-
-$sql = "SELECT distinct rodada from partida";
-$rs = mysql_query($sql) or die(mysql_error());
-$rodadas = array();
-$i = 0;
-while ($row = mysql_fetch_array($rs)) {
-    $rodadas[$i++] = $row;
+    if(!$table[$row['rodada']]) $table[$row['rodada']] = array();
+    array_push($table[$row['rodada']],$row);
 }
 ?>
 <div class="box_first">
     <h5>Escolha uma Rodada</h5>
     <div class="rodadas">
         <?
-        $i=0;
-        while($i < count($rodadas)){
-           echo " <div class='rodada'><a href='#' class='passada'>".utf8_encode($rodadas[$i]['rodada'])."</a></div>";
+        $i=1;
+        while($i < count($table)){
+           echo " <div class='rodada'><a href='?acao=simulador&rodada=".utf8_encode($table[$i]['1']['rodada'])."' class='".utf8_encode($table[$i]['1']['passou'])."'>".
+                   utf8_encode(str_pad($table[$i]['1']['rodada'],2,'0', STR_PAD_LEFT))."</a></div>\n";
            $i++;
         }
         ?>
@@ -70,15 +77,32 @@ while ($row = mysql_fetch_array($rs)) {
 <div class="box">
     <div class="partida">
         <form name="placar">
-            <img src="img/america-mg_45x45.png" title="América-MG" alt="América-MG" class="mandante"/>
-            <input type="text" name="mandante" id="mandante">
-            <img src="img/versus.png" class="versus"/>
-            <input type="text" name="visitante" id="visitante">
-            <img src="img/america-mg_45x45.png" title="América-MG" alt="América-MG" class="visitante"/>
-        </form>
-        <div class="info">
-            <p class="label">Data:</p><p>21/10/2011</p>
-            <p>
-        </div>
+        <?
+        $i=0;
+        while($i < count($table[$rodada])){
+            
+            $brasao_mandante = "img/".utf8_encode($table[$rodada][$i]['mandante_brasao']);
+            $brasao_visitante = "img/".utf8_encode($table[$rodada][$i]['visitante_brasao']);
+            $mandante_sigla = utf8_encode($table[$rodada][$i]['mandante_sigla']);
+            $visitante_sigla = utf8_encode($table[$rodada][$i]['visitante_sigla']);
+            $mandante_nome = utf8_encode($table[$rodada][$i]['mandante_nome']);
+            $visitante_nome = utf8_encode($table[$rodada][$i]['visitante_nome']);            
+            $gols_mandante = utf8_encode($table[$rodada][$i]['mandante_gols']);
+            $gols_visitante = utf8_encode($table[$rodada][$i]['visitante_gols']);
+            $data = utf8_encode($partidas_por_rodada[$rodada][$i]['data_formatada']);
+            
+            echo "<img src='".$brasao_mandante."' title='".$mandante_nome."' alt='".$mandante_nome."' class='mandante'/>";
+            echo "<input type='text' name='mandante' id='mandante' value=".$gols_mandante.">";
+            echo "<img src='img/versus.png' class='versus'/>";
+            echo "<input type='text' name='visitante' id='visitante' value=".$gols_mandante.">";
+            echo "<img src='".$brasao_visitante."' title='".$visitante_nome."' alt='".$visitante_nome."' class='mandante'/>";
+            echo "<div class='info'>";
+            echo "<p class='label'>".$data."</p>";
+            echo "<p>";
+            echo "</div>";
+            $i++;
+        }
+        ?>        
+        </form>            
     </div>
 </div>
