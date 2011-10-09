296a9422577f473e06496e905a8df76baa1c51bd
isacsandim@gmail.com <isacsandim@gmail.com@39385b1a-61bc-3cca-02f7-3c442251d249>
2011-10-09 18:56:53 +0000

    modoficando arquivos area administrativa

--- tp1-brasileirao/admin/executa_alterar_detalhes_partida.php
+++ tp1-brasileirao/admin/executa_alterar_detalhes_partida.php
@@ -1,25 +1,81 @@
 <?
-	include "verifica.php";
-        $id          = $_POST["id"];   
-	$mandante    = $_POST["mandante"];
-	$visitante   = $_POST["visitante"];
-	$estadio     = $_POST["estadio"];
-	$rodada      = $_POST["rodada"];
-	$data 	     = $_POST["data"];
-	$hora	     = $_POST["hora"];
-	$data_hora = implode(" ",array($data,$hora));	
+include "verifica.php";
+$id = $_POST["id"];
+$numero_gols_mandante = $_POST["numero_gols_mandante"];
+$numero_gols_visitante = $_POST["numero_gols_visitante"];
+$numero_faltas_mandante = $_POST["numero_faltas_mandante"];
+$numero_faltas_visitante = $_POST["numero_faltas_visitante"];
+$numero_cartoes_mandante = $_POST["numero_cartoes_mandante"];
+$numero_cartoes_visitante = $_POST["numero_cartoes_visitante"];
 
-	if ($mandante !="" && $visitante !="" && $estadio !="" && $data_hora !="" && $rodada !="") {
+for ($i = 0; $i < $numero_gols_mandante; $i++) {
+    $jogador_id = $_POST['tabela_gols_mandante' . $i . '_jogador_id'];
+    $tipo = $_POST['tabela_gols_mandante' . $i . '_tipo'];
+    $tempo = $_POST['tabela_gols_mandante' . $i . '_tempo_id'];
+    if ($jogador_id != "" && $tipo != "" && $tempo != "" && $id != "") {
+    $sql = "INSERT INTO gol (minutos,tipo,partida_id,jogador_id) 
+            values ('$tempo','$tipo','$id','$jogador_id')";
+    //echo $sql."<br/>";
+    mysql_query($sql);
+    }
+}
 
-		$sql = "UPDATE partida 
-                        SET mandante_id='$mandante',visitante_id='$visitante',estadio_id='$estadio',data='$data_hora',rodada='$rodada' 
-                        WHERE id='$id'";
-		mysql_query($sql) or die(mysql_error());
-		echo "Alteração feita com sucesso. Clique <a href='?acao=listar_partidas'>aqui</a>.";
+for ($i = 0; $i < $numero_gols_visitante; $i++) {
+    $jogador_id = $_POST['tabela_gols_visitante' . $i . '_jogador_id'];
+    $tipo = $_POST['tabela_gols_visitante' . $i . '_tipo'];
+    $tempo = $_POST['tabela_gols_visitante' . $i . '_tempo_id'];
+    if ($jogador_id != "" && $tipo != "" && $tempo != "" && $id != "") {
+    $sql = "INSERT INTO gol (minutos,tipo,partida_id,jogador_id) 
+            values ('$tempo','$tipo','$id','$jogador_id')"; 
+    //echo $sql."<br/>";
+    mysql_query($sql);
+    }
+}
 
-	}else{
+for ($i = 0; $i < $numero_faltas_mandante; $i++) {
+    $jogador_id = $_POST['tabela_faltas_mandante' . $i . '_jogador_id'];
+    $tempo = $_POST['tabela_faltas_mandante' . $i . '_tempo_id'];
+    if ($jogador_id != "" && $tempo != "" && $id != "") {
+    $sql = "INSERT INTO falta (minutos,partida_id,jogador_id) 
+            values ('$tempo','$id','$jogador_id')"; 
+    //echo $sql."<br/>";
+    mysql_query($sql);
+    }
+}
 
-		echo "Alteração cancelada.";
-	}
+for ($i = 0; $i < $numero_faltas_visitante; $i++) {
+    $jogador_id = $_POST['tabela_faltas_visitante' . $i . '_jogador_id'];
+    $tempo = $_POST['tabela_faltas_visitante' . $i . '_tempo_id'];
+    if ($jogador_id != "" && $tempo != "" && $id != "") {    
+    $sql = "INSERT INTO falta (minutos,partida_id,jogador_id) 
+            values ('$tempo','$id','$jogador_id')"; 
+    //echo $sql."<br/>";
+    mysql_query($sql);
+    }
+}
 
+for ($i = 0; $i < $numero_cartoes_mandante; $i++) {
+    $jogador_id = $_POST['tabela_cartoes_mandante' . $i . '_jogador_id'];
+    $tipo = $_POST['tabela_cartoes_mandante' . $i . '_tipo'];
+    $tempo = $_POST['tabela_cartoes_mandante' . $i . '_tempo_id'];
+    if ($jogador_id != "" && $tipo != "" && $tempo != "" && $id != "") {    
+    $sql = "INSERT INTO cartao (minutos,tipo,partida_id,jogador_id) 
+            values ('$tempo','$tipo','$id','$jogador_id')"; 
+    //echo $sql."<br/>";
+    mysql_query($sql);
+    }
+}
+
+for ($i = 0; $i < $numero_cartoes_visitante; $i++) {
+    $jogador_id = $_POST['tabela_cartoes_visitante' . $i . '_jogador_id'];
+    $tipo = $_POST['tabela_cartoes_visitante' . $i . '_tipo'];
+    $tempo = $_POST['tabela_cartoes_visitante' . $i . '_tempo_id'];
+    if ($jogador_id != "" && $tipo != "" && $tempo != "" && $id != "") {    
+    $sql = "INSERT INTO cartao (minutos,tipo,partida_id,jogador_id) 
+            values ('$tempo','$tipo','$id','$jogador_id')"; 
+    //echo $sql."<br/>";
+    mysql_query($sql);
+    }
+}
+echo "Alteração feita com sucesso. Clique <a href='?acao=listar_partidas'>aqui</a>.";
 ?>
--- tp1-brasileirao/simulador.php
+++ tp1-brasileirao/simulador.php
@@ -1,4 +1,8 @@
 <?
+$rodada = $_GET["rodada"];
+if (!$rodada) {
+    $rodada = 1;
+}
 include "admin/conecta.php";
 $sql = "drop table if exists `tp1_bd`.`partida_tmp`";
 $qry = mysql_query($sql) or die(mysql_error());
@@ -14,5 +18,67 @@ $sql = "CREATE TABLE  `tp1_bd`.`partida_tmp` (
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8 select * from partida";
 $qry = mysql_query($sql) or die(mysql_error());
 
+$sql = "drop table if exists `tp1_bd`.`time_tmp`";
+$qry = mysql_query($sql) or die(mysql_error());
 
+$sql = "CREATE TABLE  `tp1_bd`.`time_tmp` (
+    `id` int(10) unsigned NOT NULL,
+  `nome` varchar(80) NOT NULL,
+  `sigla` varchar(3) NOT NULL,
+  `estado` varchar(80) NOT NULL,
+  `brasao` varchar(255) NOT NULL,
+  `gols_pro` int(10) unsigned NOT NULL DEFAULT '0',
+  `gols_contra` int(10) unsigned NOT NULL DEFAULT '0',
+  `total_faltas` int(10) unsigned NOT NULL DEFAULT '0',
+  `tecnico_id` int(10) unsigned NOT NULL DEFAULT '1',
+  `vitorias` int(10) unsigned NOT NULL DEFAULT '0',
+  `derrotas` int(10) unsigned NOT NULL DEFAULT '0',
+  `empates` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MEMORY DEFAULT CHARSET=utf8 select * from time";
+$qry = mysql_query($sql) or die(mysql_error());
+
+$sql = "SELECT * FROM partida ORDER BY data";
+$rs = mysql_query($sql) or die(mysql_error());
+$table = array();
+$i = 0;
+while ($row = mysql_fetch_array($rs)) {
+    $table[$i++] = $row;
+}
+
+$sql = "SELECT distinct rodada from partida";
+$rs = mysql_query($sql) or die(mysql_error());
+$rodadas = array();
+$i = 0;
+while ($row = mysql_fetch_array($rs)) {
+    $rodadas[$i++] = $row;
+}
 ?>
+<div class="box_first">
+    <h5>Escolha uma Rodada</h5>
+    <div class="rodadas">
+        <?
+        $i=0;
+        while($i < count($rodadas)){
+           echo " <div class='rodada'><a href='#' class='passada'>".utf8_encode($rodadas[$i]['rodada'])."</a></div>";
+           $i++;
+        }
+        ?>
+    </div><!--End rodadas-->
+</div><!--End box_first-->
+
+<div class="box">
+    <div class="partida">
+        <form name="placar">
+            <img src="img/america-mg_45x45.png" title="América-MG" alt="América-MG" class="mandante"/>
+            <input type="text" name="mandante" id="mandante">
+            <img src="img/versus.png" class="versus"/>
+            <input type="text" name="visitante" id="visitante">
+            <img src="img/america-mg_45x45.png" title="América-MG" alt="América-MG" class="visitante"/>
+        </form>
+        <div class="info">
+            <p class="label">Data:</p><p>21/10/2011</p>
+            <p>
+        </div>
+    </div>
+</div>
