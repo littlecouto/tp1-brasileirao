9b339f2307703fb89dfa327c9e250de358f2c076
isacsandim@gmail.com <isacsandim@gmail.com@39385b1a-61bc-3cca-02f7-3c442251d249>
2011-10-09 13:26:26 +0000

    modoficando arquivos area administrativa

---
+++ tp1-brasileirao/admin/executa_alterar_detalhes_partida.php
@@ -0,0 +1,25 @@
+<?
+	include "verifica.php";
+        $id          = $_POST["id"];   
+	$mandante    = $_POST["mandante"];
+	$visitante   = $_POST["visitante"];
+	$estadio     = $_POST["estadio"];
+	$rodada      = $_POST["rodada"];
+	$data 	     = $_POST["data"];
+	$hora	     = $_POST["hora"];
+	$data_hora = implode(" ",array($data,$hora));	
+
+	if ($mandante !="" && $visitante !="" && $estadio !="" && $data_hora !="" && $rodada !="") {
+
+		$sql = "UPDATE partida 
+                        SET mandante_id='$mandante',visitante_id='$visitante',estadio_id='$estadio',data='$data_hora',rodada='$rodada' 
+                        WHERE id='$id'";
+		mysql_query($sql) or die(mysql_error());
+		echo "Alteração feita com sucesso. Clique <a href='?acao=listar_partidas'>aqui</a>.";
+
+	}else{
+
+		echo "Alteração cancelada.";
+	}
+
+?>
